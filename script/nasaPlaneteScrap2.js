8b77a8640f0596462b38b70dcf136a8408832a17
celes <debliquy.cameron@gmail.com>
2024-05-09 17:48:37 +0200

    feat: correction bug ultra zoom + update screen

--- script/nasaPlaneteScrap2.js
+++ script/nasaPlaneteScrap2.js
@@ -24,11 +24,17 @@ function wait(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
         const planetUrl = exoplanetsDict[planetName];
         const browser = await puppeteer.launch(
         {
-            headless: false,
-            args: ['--use-gl=desktop', '--enable-webgl', '--window-size=1920,1080']
+            headless: true,
+            args: ['--use-gl=desktop', '--enable-webgl', '--window-size=1280,800',
+            '--start-fullscreen',  // Lance le navigateur en plein écran
+            '--high-dpi-support=1',  // Active le support haute DPI
+            '--device-scale-factor=2'
+            ]
         });
         const page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1080 });
+        await page.setViewport({ width: 1280, height: 800, deviceScaleFactor: 1 });
+        
+        
         
         try 
         {
@@ -59,17 +65,39 @@ function wait(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
             const frameHandle = await page.$('iframe');
             const frame = await frameHandle.contentFrame();
 
-            //gestion shadow
-            await frame.waitForSelector('.loading', { hidden: true });
-            const footerDivs = await frame.$$('.footerSVGDiv');
-            if (footerDivs.length >= 2) {
-                await footerDivs[0].click();
-            }
+            //gestion shadow pour 1920x1080
+            // await frame.waitForSelector('.loading', { hidden: true });
+            // const footerDivs = await frame.$$('.footerSVGDiv');
+            // if (footerDivs.length >= 2) {
+            //     await footerDivs[0].click();
+            // }
+      
             
-            console.log("test")
-            await page.waitForSelector('#dropUpId .tab.active.notempty .icon.active');
-            await page.click('#dropUpId .tab.active.notempty .icon.active');
-            console.log("test")
+            //reset position
+            await frame.waitForSelector('.headerButton');
+            await frame.click('.headerButton');
+            await frame.evaluate(() => {
+                const menuItems = Array.from(document.querySelectorAll('nav.siteNav ul > li'));
+                const settingsItem = menuItems.find(item => item.textContent.includes('Settings'));
+                if (settingsItem) {
+                    settingsItem.click();
+                }
+            });
+            await frame.evaluate(() => {
+                const settingsEntries = Array.from(document.querySelectorAll('.settingsEntry'));
+                if (settingsEntries.length >= 4) { // Assurez-vous qu'il y a au moins quatre entrées
+                    const floodLightingSwitch = settingsEntries[3].querySelector('.switch input[type="checkbox"]'); // Sélectionnez le commutateur dans le quatrième élément
+                    if (floodLightingSwitch) {
+                        floodLightingSwitch.click(); // Cliquez sur le commutateur
+                    }
+                }
+            });
+            await frame.click('.settings .headerBar .headerRight .closeButton'); 
+            await frame.click('.mainMenu .headerBar .headerRight .headerButton');
+            await frame.waitForSelector('#compareBtnObj');
+            await frame.click('#compareBtnObj');
+            await frame.waitForSelector('.menuOptions', { visible: true });
+            await frame.click('.menuItem.planet');
 
             //suppréssion de l'ui
             await frame.evaluate(() => {
@@ -77,8 +105,6 @@ function wait(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
                 uiElements.forEach(el => el.style.display = 'none');
             });
 
-
-            
 
             //screenshot
             const canvas = await frame.$('canvas');
